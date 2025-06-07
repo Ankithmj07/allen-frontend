e65720d52cffdcd02320e07ebaa5edab08219c59
Ankithmj07 <mjankith007@gmail.com>
2025-06-07 15:42:47 +0530

    first commit

---
+++ src/constants/menuItems.ts
@@ -0,0 +1,172 @@
+const menuItems = [
+  {
+    name: 'Courses',
+    options: [
+      {
+        name: 'NEET',
+        link: '#',
+        subOptions: [
+          { name: 'Class 11', link: '#' },
+          { name: 'Class 12', link: '#' },
+          { name: 'Class 12 Plus', link: '#' }
+        ]
+      },
+      {
+        name: 'JEE',
+        link: '#',
+        subOptions: [
+          { name: 'Class 11', link: '#' },
+          { name: 'Class 12', link: '#' },
+          { name: 'Class 12 Plus', link: '#' }
+        ]
+      },
+      {
+        name: 'Class 6-10',
+        link: '#',
+        subOptions: [
+          { name: 'Class 6', link: '#' },
+          { name: 'Class 7', link: '#' },
+          { name: 'Class 8', link: '#' },
+          { name: 'Class 9', link: '#' },
+          { name: 'Class 10', link: '#' }
+        ]
+      },
+      {
+        name: 'View All Options',
+        link: '#',
+        subOptions: [
+          { name: 'Online Courses', link: '#' },
+          { name: 'Offline Courses', link: '#' },
+          { name: 'Distance Learning', link: '#' },
+          { name: 'Hindi Medium Courses', link: '#' },
+          { name: 'International Olympiad', link: '#' }
+        ]
+      }
+    ]
+  },
+  {
+    name: 'Test Series',
+    options: [
+      {
+        name: 'NEET',
+        link: '#',
+        subOptions: [
+          { name: 'Class 11', link: '#' },
+          { name: 'Class 12', link: '#' },
+          { name: 'Class 12 Plus', link: '#' }
+        ]
+      },
+      {
+        name: 'JEE (Main + Advanced)',
+        link: '#',
+        subOptions: [
+          { name: 'Class 11', link: '#' },
+          { name: 'Class 12', link: '#' },
+          { name: 'Class 12 Plus', link: '#' }
+        ]
+      },
+      {
+        name: 'JEE Main',
+        link: '#',
+        subOptions: [
+          { name: 'Class 11', link: '#' },
+          { name: 'Class 12', link: '#' },
+          { name: 'Class 12 Plus', link: '#' }
+        ]
+      }
+    ]
+  },
+  {
+    name: 'Results',
+    options: [
+      {
+        name: 'JEE Main 2025',
+        link: '#'
+      },
+      {
+        name: 'NEET',
+        link: '#',
+        subOptions: [
+          { name: '2024', link: '#' },
+          { name: '2023', link: '#' },
+          { name: '2022', link: '#' }
+        ]
+      }
+    ]
+  },
+  {
+    name: 'Study Materials',
+    options: [
+      {
+        name: 'JEE Mains',
+        link: '#',
+        subOptions: [
+          { name: 'Physics', link: '#' },
+          { name: 'Chemistry', link: '#' },
+          { name: 'Maths', link: '#' },
+          { name: 'NCERT Solutions', link: '#' },
+          { name: 'Sample Papers', link: '#' },
+          { name: 'Previous Year Papers', link: '#' }
+        ]
+      },
+      {
+        name: 'JEE Advanced',
+        link: '#',
+        subOptions: [
+          { name: 'Physics', link: '#' },
+          { name: 'Chemistry', link: '#' },
+          { name: 'Maths', link: '#' },
+          { name: 'Mock Tests', link: '#' },
+          { name: 'NCERT Solutions', link: '#' },
+          { name: 'Revision Notes', link: '#' }
+        ]
+      },
+      {
+        name: 'NEET',
+        link: '#',
+        subOptions: [
+          { name: 'Physics', link: '#' },
+          { name: 'Chemistry', link: '#' },
+          { name: 'Biology', link: '#' },
+          { name: 'NCERT Solutions', link: '#' },
+          { name: 'Practice Questions', link: '#' },
+          { name: 'Revision Notes', link: '#' }
+        ]
+      },
+      {
+        name: 'NCERT Solutions',
+        link: '#',
+        subOptions: [
+          { name: 'Class 6', link: '#' },
+          { name: 'Class 7', link: '#' },
+          { name: 'Class 8', link: '#' },
+          { name: 'Class 9', link: '#' },
+          { name: 'Class 10', link: '#' },
+          { name: 'Class 11-12', link: '#' }
+        ]
+      }
+    ]
+  },
+  {
+    name: 'Books',
+    options: [
+      {
+        name: 'ALLEN e-Store',
+        link: '#'
+      }
+    ]
+  },
+  {
+    name: 'More',
+    options: [
+      { name: 'Allen for Schools', link: '#' },
+      { name: 'About ALLEN', link: '#' },
+      { name: 'Blogs', link: '#' },
+      { name: 'News', link: '#' },
+      { name: 'Careers', link: '#' }
+    ]
+  }
+];
+
+
+export default menuItems;
